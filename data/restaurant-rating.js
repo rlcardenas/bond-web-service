a819b46be5431be6f43e0d1ab927139f5cc49851
rlcardenas <72461018+rlcardenas@users.noreply.github.com>
2021-03-25 00:08:24 -0700

    update

---
+++ data/restaurant-rating.js
@@ -0,0 +1,97 @@
+{
+	"title": "Restaurants by Rating",
+	"restaurants":[
+			  {
+				"Name": "Pizzeria Credo",
+				"Phone":"+1 206-402-3784",
+				"Address": "4520 California Ave SW, Seattle, WA 98116-4111",
+				"Rating":5,
+				"Cuisine":"Italian",
+                "Image": "pizzeria_credo.jpg"
+			  },
+			  
+			  {
+				"Name": "Chan",
+				"Phone":"+1 206-354-3564",
+				"Address": "86 Pine St In the Inn at the Market courtyard, Seattle, WA 98101-1531",
+				"Rating":4.5,
+				"Cuisine":"Asian",
+                "Image": "chan.jpg"
+			  },
+
+			  {
+				"Name": "Cafe Munir",
+				"Phone":"+1 206-783-4190",
+				"Address": "2408 NW 80th St, Seattle, WA 98117-4449",
+				"Rating":4.5,
+				"Cuisine":"Lebanese",
+                "Image": "cafe-munir.jpg"
+			  },
+
+			  {
+				"Name": "Japonessa Sushi Cocina",
+				"Phone":"+1 206-971-7979",
+				"Address": "1400 1st Ave, Seattle, WA 98101-2004",
+				"Rating":4.5,
+				"Cuisine":"Japanese",
+                "Image": "japonessa.jpg"
+			  },
+
+			  {
+				"Name": "Frelard Tamales",
+				"Phone":"+1 206-370-9296",
+				"Address": "6412 Latona Ave NE Walk Up Window, Seattle, WA 98115-6557",
+				"Rating":4.5,
+				"Cuisine":"Mexican",
+                "Image": "frelard_tamales.jpg"
+			  },
+
+			  {
+				"Name": "Xi'an Noodles",
+				"Phone":"+1 206-522-8888",
+				"Address": "5259 University Way NE, Seattle, WA 98105-3546",
+				"Rating":4.5,
+				"Cuisine":"Chinese",
+                "Image": "xian_noodles.jpg"
+			  },
+
+			  {
+				"Name": "La Carta de Oaxaca",
+				"Phone":"+1 206-782-8722",
+				"Address": "5431 Ballard Ave NW, Seattle, WA 98107-4052",
+				"Rating":4.5,
+				"Cuisine":"Mexican",
+                "Image": "lacarta_deoaxaca.jpg"
+			  },
+
+			  {
+				"Name": "Kamonegi",
+				"Phone":"+1 206-632-0185",
+				"Address": "1054 N 39th St, Seattle, WA 98103-7921",
+				"Rating":4.5,
+				"Cuisine":"Japanese",
+                "Image": "kamonegi.jpg"
+			  },
+
+			  			  
+			  {
+				"Name": "JuneBaby",
+				"Phone":"+1 206-257-4470",
+				"Address": "2122 NE 65th St, Seattle, WA 98115-6936",
+				"Rating":4.0,
+				"Cuisine":"American",
+                "Image": "june_baby.jpg"
+			  },
+
+			  {
+				"Name": "Pineapple Bistro & Bar",
+				"Phone":"+1 206-284-4955",
+				"Address": "300 Roy St, Seattle, WA 98109-4114",
+				"Rating":3.5,
+				"Cuisine":"American",
+                "Image": "pineapple_bistro.jpg"
+			  }
+			  
+
+			]
+}
